0339b6d6b898b1686c321f76faa53afd4884f6d5
mrmurilo75 <mrmurilo75@gmail.com>
2021-03-19 05:51:34 +0000

    @src/main : start imlpementing initialize()

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "card.h"
+#include "card.c"
 #include "linkedlist.c"
+#include "SortedList.c"
+
+#define TODO 1
+#define DOING 2
+#define DONE 3
+
+#define BYALL 1
+#define BYAUTHOR 2
+#define BYCREATOR 3
 
 int initialize(void);		// get .txt files from disk into memory and fill pointers
 
@@ -81,10 +90,158 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
+int numberOfCards=0;
+
+CardList byAll;
+CardList byAuthor;
+CardList byCreation;
+
+void createCleanLists(void);
+int putInListing(card* next);
 
 int initialize(void){
 		// get .txt files from disk into memory and fill pointers
+	int err=0;
+	FILE *fcards = fopen("cards.bin", "r+b");
+	if(fcards == NULL){
+		fcards = fopen("cards.bin", "w+b");
+		return 0;
+	}
+	createCleanLists();
+	for(card* next=fnewCard(fcards); fnewCards!=NULL; next=fnewCard(fcards))
+		if( (err = putInListing(next)) ) {
+			putError(err);
+			return err;
+		}
+
 	return	0;
+}
+
+void createCleanLists(){
+	byAll = (CardList) malloc(sizeof(cardNode));
+	byAuthor = (CardList) malloc(sizeof(cardNode));
+	byCreation = (CardList) malloc(sizeof(cardNode));
+	
+	byAll->value = NULL;
+	byAuthor->value = NULL;
+	byCreation->value = NULL;
+}
+
+int putByAll(cardNode* input, cardNode* now, cardNode* prev);
+int putByAuthor(cardNode* input, cardNode* now, cardNode* prev);
+int putByCreator(cardNode* input, cardNode* now, cardNode* prev);
+
+int putInListing(card* next){
+	cardNode* input = (cardNode*) malloc(sizeof(cardNode));
+	input->value = next;
+
+	int err=0;
+	if( (err = putByAll(input, byAll, NULL)) )
+		return err;
+	if( (err = putByAuthor(input, byAuthor, NULL)) )
+		return err;
+	if( (err = putByCreator(input, byCreator, NULL)) )
+		return err;
+
+	return err;
+}
+
+
+int putIn(byte list, cardNode* now, cardNode* prev, cardNode* next);
+
+cardNode* byAllLastToDo = NULL;
+
+int putByAll(cardNode* input, cardNode* now, cardNode* prev){
+	if(now->value == NULL)		// list is empty or we've reached the end
+		return putIn(BYALL, input, prev, now);
+
+	if(input->value->column < now->value->column){		// when ordering by all follow TODO -> DOING -> DONE
+		if(now->value->column == DOING)
+			byAllLastToDo = input;
+		return putIn(BYALL, input, prev, now);
+	}
+
+	if(input->value->column == TODO){
+		if(input->value->priority > now->value->priority || (input->value->priority == now->value->priority && input->value->creation <= now->value->creation))
+			return putIn(BYALL, input, prev, now);
+		return putByAll(input, now->nextByAll, now);
+	}
+
+	if(input->value->column == DOING){
+		if(now->nextByAuthor == NULL && byAllPrevByAuthor != NULL)
+			return putByAll(input, byAuthor, byAllLastToDo);	// the byAuthor list first element coincides with the first DOING element in byAll
+		if(input->value->author >= now->value->author)
+			return putIn(BYALL, input, prev, now);
+		return putByAll(input, now->nextByAll, now);
+	}
+
+	// if(input->value->column == DONE){
+		if(input->value->author >= now->value->author)
+			return putIn(BYALL, input, prev, now);
+		return putByAll(input, now->nextByAll, now);
+	//}
+
+	return -1;
+
+}
+
+int putByAuthor(cardNode* input, cardNode* now, cardNode* prev){
+	// IMPLEMENT 
+	// similar to putByAll
+	return -1;
+}
+
+int putByCreator(cardNode* input, cardNode* now, cardNode* prev){
+	// IMPLEMENT 
+	// similar to putByAll
+	return -1;
+}
+
+int putIn(byte list, cardNode* now, cardNode* prev, cardNode* next){
+	if(prev == NULL)		// now is the first node
+		switch(list){
+			case BYALL: 
+				byAll = input;
+				break;
+			case BYAUTHOR:
+				byAuthor = input;
+				break;
+			case BYCREATOR: 
+				byCreator = input;
+				break;
+			default:
+				return -1;
+		}
+	else
+		switch(list){
+			case BYALL: 
+				prev->nextByAll = input;
+				break;
+			case BYAUTHOR:
+				prev->nextByAuthor = input;
+				break;
+			case BYCREATOR: 
+				prev->nextByCreator = input;
+				break;
+			default:
+				return -1;
+		}
+	if(now != NULL)
+		switch(list){
+			case BYALL: 
+				input->nextByAll = now;
+				break;
+			case BYAUTHOR:
+				input->nextByAuthor = now;
+				break;
+			case BYCREATOR: 
+				input->nextByCreator = now;
+				break;
+			default:
+				return -1;
+		}
+
+	return 0;
 }
 
 int get_option(void){
