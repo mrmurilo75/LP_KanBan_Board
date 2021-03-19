b83d5ae8f3ac313d15a4e7a06263848c8c7fc872
mrmurilo75 <mrmurilo75@gmail.com>
2021-03-19 01:41:19 +0000

    @src/card.h : add header guard & add interface

--- src/card.h
+++ src/card.h
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
+#ifndef BYTE_DEFINED
+typedef char byte;
+#endif	// BYTE_DEFINED
+
+#ifndef CARD_H_INCLUDED
+
 #define TODO 1
 #define DOING 2
 #define DONE 3
 
-typedef char byte;
+#define CARD_H_INCLUDED
 
 typedef struct {
 	long int text, author; //for use with ftell/fseek and for easier sorting
 	unsigned long int creation, due, conclusion;
-	short int priority;
+	byte priority;
 	byte column;
 
 	//fpos_t text, author; //for use with fgetpos/fsetpos if necessary 
 } card;
+
+card* fnewCard(FILE* stream);	// malloc space for a card and fill from stream
+
+#endif	//CARD_H_INCLUDED
