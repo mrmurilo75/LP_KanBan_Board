3cdf9c1f51dd9e924fe322a7bd033be57683af81
mrmurilo75 <mrmurilo75@gmail.com>
2021-03-19 18:13:10 +0000

    @src/card.c : implement current card.h

--- src/card.c
+++ src/card.c
@@ -1,3 +1,9 @@
 #include "card.h"
 
+int fnewCard(FILE * stream){
+	if(stream == NULL)
+		return -1;
 
+	card *newCard = (card *) malloc(sizeof(card));
+	return (fread(newCard, sizeof(card), 1, stream) - 1);
+}
